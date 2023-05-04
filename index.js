bb8265abf9323ece5e04131899cd549857e1b659
Timothy Ryon <tim@timryon.com>
2023-05-03 20:30:46 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+
+function saturdayFun(funActivity="roller-skate") {
+    return `This Saturday, I want to ${funActivity}!`;
+}
+
+function mondayWork(funWeek="go to the office") {
+    return `This Monday, I will ${funWeek}.`;
+}
+
+function wrapAdjective(style = "*"){
+    return function(adjective="special") {
+        return `You are ${style}${adjective}${style}!`;
+    } 
+}
+
+/*
+wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a hard worker");
+
+let defaultActivity = "roller-skate";
+
+function saturdayFun(funActivity) {
+    console.log(funActivity);
+    if (funActivity === undefined) {
+    return (`This Saturday, I want to ${defaultActivity}!`);
+    } 
+    else {
+    return (`This Saturday, I want to ${funActivity}!`);
+    }
+}
+*/
+
